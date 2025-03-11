d7d96a48202c353f91d1740f3d4a26bca6bc3c33
Nitai Heeb <n.heeb@fz-juelich.de>
2025-03-11 13:08:36 +0100

    plugin test

---
+++ test.sh
@@ -0,0 +1,6 @@
+curl -X POST https://sandbox.zenodo.org/oauth/token \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+--data grant_type=refresh_token \
+--data refresh_token=sdDpTb71goRYQblPd2ldlyUWhrKNzJ \
+--data client_id=QJ8Q9GBI78uOdNmVNK1Vd0oAOJHqmYGvxRxiSFxt \
+--data client_secret=nGuOqoDtd2tckP6lmQS3If3cY39lPLKLU8skcv72JeowNupMD2bnLparsGO9
